7859e070e442b04f2389fd5fbcd227514ce2bca9
Patipol Wangjaitham <patipol.wa@ku.th>
2019-01-24 13:19:25 +0700

    Initial Commit

---
+++ SayWhatILike/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SayWhatILike
+//
+//  Created by Patipol Wangjaitham on 24/1/2562 BE.
+//  Copyright © 2562 Patipol Wangjaitham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
